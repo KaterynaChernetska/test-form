f714c47b83ff65cfc32f4795363dc3e15f5f335b
Kateyna <vesnavved@gmail.com>
2024-06-30 20:08:06 +0300

    add header and body styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import "./Header.scss";
+import MobileLogo from "../../assets/icons/logo_small.svg";
+import Powercode from "../../assets/icons/POWERCODE.svg";
+import Academy from "../../assets/icons/ACADEMY.svg";
+import Calendar from "../../assets/icons/calendar.svg";
+import Clock from "../../assets/icons/clock.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <a className="logo-link" href="/">
+        <img src={MobileLogo} className="logo-svg" alt="owercode logo" />
+        <div className="logo-container">
+          <img src={Powercode} className="powercode-logo-svg" alt="Powercode" />
+          <img src={Academy} className="academy-logo-svg" alt="Academy" />
+        </div>
+      </a>
+      <ul className="header-date-container">
+        <li>
+          <img src={Calendar} className="calendar-svg" alt="Calendar-icon" />
+          28 декабря
+        </li>
+        <li>
+          <img src={Clock} className="clock-svg" alt="clock-icon" />
+          3,5 часа
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
